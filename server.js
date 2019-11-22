3fd5da0a2f6c40c9892501f433247dfeadb1b172
jokerjester <jokerjester@me.com>
2019-11-23 05:21:52 +0700

    change port

--- server.js
+++ server.js
@@ -35,6 +35,8 @@ app.get('/webhooks', (req, res) => {
         res.sendStatus(200)
 })
 
-app.listen(3000, () => {
-    console.log('Start server at port 3000.')
-})
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
