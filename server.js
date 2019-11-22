376a685f0dd47f290282bf886a9e84594cd3821e
jokerjester <jokerjester@me.com>
2019-11-23 05:32:26 +0700

    added reply function

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express()
 const asm1 = require('./assignment1')
 const api_helper = require('./helpers/APIhelper')
 const status = require('http-status');
+const bodyParser = require('body-parser')
+const request = require('request')
 
 const  ggPlacrUri = 'https://maps.googleapis.com/maps/api/place/nearbysearch/'
 const  respType = 'json'
@@ -31,7 +33,38 @@ app.get('/ans2', (req, res) => {
 })
 
 // assignment3
-app.post('/webhooks', (req, res) => res.sendStatus(200))
+const replyToken = 'DNrLFw6H0xJ/iSkv0IdI6QnCVOD14pUX/ie0zZel0IHhapJAqb3xUSkI3XAUlx0wUiNSyV4KRGsLQ0irygjnSpNjyQeZhi+uZqzW1cMWAkOOEmKocHvOS/xw9mFVFXJez9GXfmWdaHouym0NYA9k4AdB04t89/1O/w1cDnyilFU='
+app.post('/webhook', (req, res) => {
+    let reply_token = req.body.events[0].replyToken
+    reply(reply_token)
+    res.sendStatus(200)
+})
+
+
+reply = function(reply_token) {
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer '.concat(replyToken)
+    }
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        messages: [{
+            type: 'text',
+            text: 'Hello'
+        },
+        {
+            type: 'text',
+            text: 'How are you?'
+        }]
+    })
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+        console.log('status = ' + res.statusCode);
+    });
+}
 
 
 var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
