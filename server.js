79ba22546f2fcff436877a5127c800c9f16d8d4b
jokerjester <jokerjester@me.com>
2019-11-23 07:58:36 +0700

    rename file

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const respType = 'json'
 const ggKey = 'AIzaSyD00nvz4mUNbto6QrUaBmKYs2vXx_wrcbw'
 
 // import helpers
-const apiHelper = require('./helpers/APIHelper')
+const apiHelper = require('./helpers/WSHelper')
 
 // import service layer
 const sampleService = require('./services/SampleService')
